9b163d944862773419f03e120bdd8e52dfe8b4c8
alix12547 <115891306+alix12547@users.noreply.github.com>
2023-09-14 23:08:07 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 import json
+import imaplib
+import datetime  # Add this import for time-based search
 
 root = tk.Tk()
 root.title("Email Search Tool")
@@ -159,6 +161,80 @@ def handle_dropdown_change(event):
 # Bind the function to the dropdown's <<ComboboxSelected>> event
 search_dropdown.bind("<<ComboboxSelected>>", handle_dropdown_change)
 
+def fetch_email_counts():
+    email = email_var.get()
+    if not email:
+        return  # No email selected
+
+    # Get the email and password from the data.json file
+    try:
+        with open("data.json", "r") as json_file:
+            email_data = json.load(json_file)
+            for entry in email_data:
+                if entry["email"] == email:
+                    password = entry["password"]
+                    break
+            else:
+                return  # Email not found in data.json
+    except (FileNotFoundError, json.JSONDecodeError):
+        return  # File not found or JSON decode error
+
+    # Connect to the IMAP server
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+
+    # Log in to the email account
+    try:
+        imap.login(email, password)
+    except imaplib.IMAP4.error:
+        text_area.delete(1.0, tk.END)  # Clear previous results
+        text_area.insert(tk.END, "Invalid email or password.")
+        return
+
+    # Fetch the counts of emails based on the selected search option
+    search_option = search_var.get()
+    search_text = search_entry.get()
+    time_value = time_entry.get()
+
+        # Construct the criteria based on the selected search option
+    # Construct the criteria based on the selected search option
+    if search_option == "all":
+        # Search for emails received within the specified time (e.g., 10 minutes)
+        time_threshold = (datetime.datetime.now() - datetime.timedelta(minutes=int(time_value)))
+        criteria = f'newer_than:{time_value}h '
+        print(criteria)
+    elif search_option == "Subject":
+        criteria = f'Subject :{search_text} newer_than:{time_value}h '
+        print(criteria)
+    elif search_option == "From":
+        criteria = f'From :"{search_text}" newer_than:{time_value}h '
+        print(criteria)
+    else:
+        # Handle invalid search_option
+        text_area.delete(1.0, tk.END)  # Clear previous results
+        text_area.insert(tk.END, "Invalid search option selected.")
+        return
+
+
+
+    try:
+        imap.select("inbox")
+        _, search_data = imap.search(None, criteria)
+        email_count = len(search_data[0].split())
+    except Exception as e:
+        # Handle the error gracefully
+        text_area.delete(1.0, tk.END)  # Clear previous results
+        text_area.insert(tk.END, f"Error: {str(e)}")
+    else:
+        text_area.delete(1.0, tk.END)  # Clear previous results
+        text_area.insert(tk.END, f"Total Emails matching criteria: {email_count}\n")
+
+    # Close the mailbox and log out
+    imap.logout()
+
+
+
+
+
 
 
 
@@ -183,17 +259,18 @@ spf_checkbox.pack(side=tk.LEFT, padx=10, pady=5)
 
 
 # Label and input field for 'Time of Search by Min'
-time_label = ttk.Label(root, text="Time of Search by Min:")
+time_label = ttk.Label(root, text="Time of Search by heure:")
 time_label.pack()
 
 time_entry = ttk.Entry(root)
-time_entry.insert(0, "10")  # Set the initial text
+time_entry.insert(0, "1")  # Set the initial text
 
 time_entry.pack()
 
 # Button for 'Check'
-check_button = ttk.Button(root, text="Check")
+check_button = ttk.Button(root, text="Check", command=fetch_email_counts)
 check_button.pack()
+
 
 # Text area for displaying results
 text_area = tk.Text(root, wrap=tk.WORD, width=40, height=10)
